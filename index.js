2fd6c37b6b3aa867c25987c2ef6e0d725b91b9f8
Ray Ashir <rayeed.ashir@gmail.com>
2021-09-01 21:00:06 -0700

    added menu questions

--- index.js
+++ index.js
@@ -1,25 +1,48 @@
-const inquirer = require ('inquirer');
+const inquirer = require('inquirer');
 
-const QUESTIONS = [
+const MENU_QUESTION = [ 
   {
-    type: 'input', 
-    message: 'option 1',
-    name: 'employee name'
-  }, 
-  {
-    type: 'input', 
-    message: 'option 2', 
-    name: 'emp name 2'
+    type: 'list', 
+    message: 'What would you like to do?',
+    name: 'userSelection', 
+    choices: ['View all departments', 'View all Roles', 'View all employees', 'Add a department', 'Add a role', 'Add an Employee', 'Update and Employee Role', 'Exit']
   }
 ];
 
 function askQuestions() {
   console.log('What would you like to do');
   inquirer
-  .prompt(QUESTIONS)
+  .prompt(MENU_QUESTION)
   .then((response) => {
     console.log(response);
-  });
+  })
 }
 
 askQuestions();
+
+
+/* WIP
+GIVEN a command-line application that accepts user input
+WHEN I start the application
+THEN I am presented with the following options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
+*/
+
+/*DONE
+*/
+
+/*NOT YET STARTED
+WHEN I choose to view all departments
+THEN I am presented with a formatted table showing department names and department ids
+WHEN I choose to view all roles
+THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
+WHEN I choose to view all employees
+THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
+WHEN I choose to add a department
+THEN I am prompted to enter the name of the department and that department is added to the database
+WHEN I choose to add a role
+THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
+WHEN I choose to add an employee
+THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
+WHEN I choose to update an employee role
+THEN I am prompted to select an employee to update and their new role and this information is updated in the database 
+*/
