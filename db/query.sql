e93ec9502952978b4776fd04e561d4b9e441427c
Ray Ashir <rayeed.ashir@gmail.com>
2021-09-01 21:27:02 -0700

    added query db

---
+++ db/query.sql
@@ -0,0 +1,9 @@
+USE employee_db
+
+select * from employee;
+select * from department;
+select * from roles;
+
+-- SELECT book_name, book_prices.price
+-- FROM favorite_books
+-- JOIN book_prices ON favorite_books.book_price = book_prices.id;
