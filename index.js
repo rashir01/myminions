102b4213daee7492d5dcf26e91a88d513158fd10
Ray Ashir <rayeed.ashir@gmail.com>
2021-09-01 20:46:25 -0700

    added package.json and .gitignore

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-const inquirer = require (inquirer);
+const inquirer = require ('inquirer');
+
+const QUESTIONS = [
+  {
+    type: 'input', 
+    message: 'option 1',
+    name: 'employee name'
+  }, 
+  {
+    type: 'input', 
+    message: 'option 2', 
+    name: 'emp name 2'
+  }
+];
+
+function askQuestions() {
+  console.log('What would you like to do');
+  inquirer
+  .prompt(QUESTIONS)
+  .then((response) => {
+    console.log(response);
+  });
+}
+
+askQuestions();
